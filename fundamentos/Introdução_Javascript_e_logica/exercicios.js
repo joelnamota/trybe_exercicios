b16d38a8f6882e75085bdaef945f3642215427ce
Denis Oliveira <joelnamota@hotmail.com>
2022-07-26 16:25:09 -0300

    exercicio condicional

---
+++ fundamentos/Introdução_Javascript_e_logica/exercicios.js
@@ -0,0 +1,10 @@
+const notaDesafio = 80
+
+if (notaDesafio >= 80) {
+    console.log("Aprovado")
+} else if (notaDesafio < 80 && notaDesafio >= 60) {
+    console.log("lista de espera")
+} else if (notaDesafio < 60) {
+    console.log("Reprovado")
+
+}
